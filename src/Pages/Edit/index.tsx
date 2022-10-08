ed4e42c8579377db7f1c6ce06f77b5dacaf7ac00
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2022-10-08 15:22:05 -0700

    Create Navigation

---
+++ src/Pages/Edit/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import { NavProps, TabParams } from '../../Components/Navigation/TabProps';
+
+interface EditProps {
+
+}
+
+const Edit = () => {
+    return (
+        <View>
+            <Text>
+                Edit Page
+            </Text>
+        </View>
+    );
+};
+
+export default Edit;
