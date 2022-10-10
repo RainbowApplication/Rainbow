dfbea24acd5660a55b89e7436d5be0121040b4be
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2022-10-10 03:59:01 -0700

    Testing: TextInput

--- src/Pages/Edit/index.tsx
+++ src/Pages/Edit/index.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
 
 interface EditProps {
 
 }
 
 const Edit: React.FC<EditProps> = ({}: EditProps) => {
+  const [text, setText] = React.useState('');
+
   return (
     <View style={styles.ViewStyle}>
       <Text style={styles.Text}>
         Hello World
-        <View style={{borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth}}></View>
       </Text>
+      <TextInput value={text} onChangeText={setText} />
     </View>
   )
 }
